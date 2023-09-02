918030fa348ba4bafcb5472fbca41906b07957f0
Rayan Perera <rayanp2004@gmail.com>
2023-09-02 11:49:11 +0530

    Initial commit

---
+++ Program (1).cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Activity___2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter amount of the bill");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            int rate;
+            Console.WriteLine("Enter the discount rate : ");
+            rate = Convert.ToInt32(Console.ReadLine());
+
+           
+            int finalprice;
+            finalprice= amount * rate / 100;
+            Console.WriteLine("Your Discount : "+finalprice);
+
+            int dprice;
+            dprice = amount - finalprice;
+            Console.WriteLine("Your Discounted Price will be :"+dprice);
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
